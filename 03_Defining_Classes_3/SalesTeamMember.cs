e2681cf37dfc94c8a71313d004fbae33f20f0dff
Joshua Tucker <thejltucker@gmail.com>
2018-10-10 10:26:09 -0400

    Added DC3

---
+++ 03_Defining_Classes_3/SalesTeamMember.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Defining_Classes_3
+{
+    public class SalesTeamMember
+    {
+    }
+}
