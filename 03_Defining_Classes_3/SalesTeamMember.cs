91a3f1e65497dbced45a2b8f2a95050fa725430e
Joshua Tucker <thejltucker@gmail.com>
2018-10-10 11:57:42 -0400

    SalesTeamRepo made and tested

--- 03_Defining_Classes_3/SalesTeamMember.cs
+++ 03_Defining_Classes_3/SalesTeamMember.cs
@@ -8,5 +8,13 @@ namespace _03_Defining_Classes_3
 {
     public class SalesTeamMember
     {
+        public SalesTeamMember(string lastName, decimal monthlySales)
+        {
+            LastName = lastName;
+            MonthlySales = monthlySales;
+        }
+
+        public string LastName { get; set; }
+        public decimal MonthlySales { get; set; }
     }
 }
