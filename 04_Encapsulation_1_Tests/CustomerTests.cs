5289cdd70e59dc21c85fb6bd30b9aac5ece92bec
Paul O'Connor <jamespauloconnor@gmail.com>
2018-10-11 10:10:28 -0400

    Getters and setters

---
+++ 04_Encapsulation_1_Tests/CustomerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using _04_Encapsulation_1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _04_Encapsulation_1_Tests
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Customer customer = new Customer();
+            customer.FirstName = "Kenn";
+            Console.WriteLine(customer.FirstName);
+
+        }
+    }
+}
